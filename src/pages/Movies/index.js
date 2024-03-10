80c7b52baedd6adb8e15aae33d1505c2b727ff97
pedrohrocha18 <phr.tech@outlook.com>
2024-03-10 09:32:01 -0300

    Feat: iniciando projeto

---
+++ src/pages/Movies/index.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+export default function Movies() {
+  return (
+    <View>
+      <Text>Movies Page</Text>
+    </View>
+  );
+}
